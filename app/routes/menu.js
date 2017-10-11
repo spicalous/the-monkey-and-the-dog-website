0eb46684ea210a64fb3a52dd50611d532f66a141
Ben Lertlumprasertkul <Ben.Lertlumprasertkul@ig.com>
2017-10-11 11:06:08 +0100

    get JSON with relative path

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return Ember.$.getJSON('/menu/menu.json');
+    return Ember.$.getJSON('menu/menu.json');
   }
 
 });
